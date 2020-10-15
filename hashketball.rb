45a7a7bb1b015fc51f3440e9ec485ded96e662b4
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-15 15:07:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -245,6 +245,7 @@ def big_shoe_rebounds
   big_shoe_hash = {}
   basketball.each do |team_origin, team_info|
     team_info.each do |team_detail, detail|
+      detail.each do |key|
       binding.pry
     end
   end
